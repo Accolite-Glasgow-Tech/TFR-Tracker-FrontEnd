637b70d1f9a6711633a08c740b865c5b7fccceec
Sourabh~ <sourabh.sahu@AccoliteDigital.com>
2022-12-17 21:39:24 +0000

    Intial Changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreMaterialModule } from './core-modules/core-material/core-material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CoreMaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/core-modules/core-material/core-material.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {
+  MatIconModule
+} from '@angular/material/icon';
+
+import {
+  MatButtonModule
+} from '@angular/material/button';
+
+import {
+  MatSidenavModule
+} from '@angular/material/sidenav';
+import {
+  MatToolbarModule
+} from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [],
+  imports:      [
+    BrowserModule, FormsModule, FlexLayoutModule, BrowserAnimationsModule,
+    MatIconModule, MatButtonModule, MatSidenavModule, MatToolbarModule,
+    CommonModule
+  ],
+  exports:[
+    FlexLayoutModule,
+    MatIconModule, MatButtonModule, MatSidenavModule, MatToolbarModule
+  ]
+})
+export class CoreMaterialModule { }
