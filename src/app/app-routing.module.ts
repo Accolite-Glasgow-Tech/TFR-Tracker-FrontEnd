2b02dbc427025c41964875c635dd27e27b5af9e9
Laura Rintoul <laura.rintoul@accolitedigital.com>
2022-12-20 12:45:20 +0000

    Added a few components onto route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { TfrsComponent } from './components/tfrs/tfrs.component';
 
 export const routes: Routes = [{
   path:'home',component: HomeComponent
 },
+{
+  path:'tfrs',component: TfrsComponent
+},
+
 { path: '**', redirectTo:'home' }];
 
 @NgModule({
