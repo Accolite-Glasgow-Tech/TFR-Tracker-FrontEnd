7345b53e683966312873f06e2d2ec2f3ade4c5ea
Laura Rintoul <laura.rintoul@accolitedigital.com>
2022-12-20 12:58:03 +0000

    Added routes for all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlertsComponent } from './components/alerts/alerts.component';
 import { HomeComponent } from './components/home/home.component';
+import { MilestonesComponent } from './components/milestones/milestones.component';
+import { PermissionsComponent } from './components/permissions/permissions.component';
+import { ReportingComponent } from './components/reporting/reporting.component';
 import { TfrsComponent } from './components/tfrs/tfrs.component';
 
 export const routes: Routes = [{
@@ -8,6 +12,18 @@ export const routes: Routes = [{
 },
 {
   path:'tfrs',component: TfrsComponent
+},
+{
+  path:'milestones',component: MilestonesComponent
+},
+{
+  path:'alerts',component: AlertsComponent
+},
+{
+  path:'reporting',component: ReportingComponent
+},
+{
+  path:'permissions',component: PermissionsComponent
 },
 
 { path: '**', redirectTo:'home' }];
