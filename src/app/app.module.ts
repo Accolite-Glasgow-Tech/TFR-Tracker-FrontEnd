1b3e0b6898aa4e96797fc253d1a6f036e0303f18
Laura Rintoul <laura.rintoul@accolitedigital.com>
2022-12-20 12:46:31 +0000

    Added dumb components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreMaterialModule } from './core-modules/core-material/core-material.module';
+import { HomeComponent } from './components/home/home.component';
+import { TfrsComponent } from './components/tfrs/tfrs.component';
+import { MilestonesComponent } from './components/milestones/milestones.component';
+import { AlertsComponent } from './components/alerts/alerts.component';
+import { ReportingComponent } from './components/reporting/reporting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TfrsComponent,
+    MilestonesComponent,
+    AlertsComponent,
+    ReportingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/tfrs/tfrs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tfrs',
+  templateUrl: './tfrs.component.html',
+  styleUrls: ['./tfrs.component.scss']
+})
+export class TfrsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
