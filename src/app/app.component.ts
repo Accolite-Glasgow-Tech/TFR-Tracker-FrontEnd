f88c7ef3f533545261649695bab92e17cc2e1767
Laura Rintoul <laura.rintoul@accolitedigital.com>
2022-12-20 15:57:34 +0000

    Added routes to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RoutesService } from './routes.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor() {}
   title = 'TFR-Management';
+  NavigatorData = RoutesService.RouteList;
 }
