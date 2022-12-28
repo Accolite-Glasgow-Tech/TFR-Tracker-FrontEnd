b5bfbb467c0a0092f917bcb74c09fc00329b0ad1
ACCOLITE\marek.tomecek <marek.tomecek@accolitedigital.com>
2022-12-28 12:56:20 +0000

    Merge with Mani

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FrequencyPickerComponent } from './components/frequency-picker/frequency-picker.component';
+import { ReportsComponent } from './components/reports/reports.component';
 
-const routes: Routes = [
-  { path: 'reports', component: FrequencyPickerComponent }
-];
+const routes: Routes = [{ path: 'reports', component: ReportsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
