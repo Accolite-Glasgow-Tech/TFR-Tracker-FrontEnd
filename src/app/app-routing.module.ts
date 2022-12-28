d806ef0e7574bd502afb23b715bf31318b83d1d2
Mani-Yazdankhah <mani.yazdankhah@accolitedigital.com>
2022-12-28 12:48:46 +0000

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FrequencyPickerComponent } from './components/frequency-picker/frequency-picker.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'reports', component: FrequencyPickerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreMaterialModule } from './core-modules/core-material/core-material.module';
+import { FrequencyPickerComponent } from './components/frequency-picker/frequency-picker.component';
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrequencyPickerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    CoreMaterialModule
+    FormsModule,
+    ReactiveFormsModule,
+    CoreMaterialModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
