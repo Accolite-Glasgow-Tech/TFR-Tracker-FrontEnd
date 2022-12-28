a8d9ecda7e5c792ba39606cc87b14ea0f448d7b7
Laura Rintoul <laura.rintoul@accolitedigital.com>
2022-12-28 09:53:14 +0000

    change of tfr id attribute to be integer only

--- src/app/components/tfr/tfr.component.ts
+++ src/app/components/tfr/tfr.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tfr',
@@ -7,10 +7,19 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./tfr.component.scss'],
 })
 export class TfrComponent implements OnInit {
-  TfrId!: String | null;
-  constructor(private route: ActivatedRoute) {}
+  TfrId!: Number;
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
-    this.TfrId = this.route.snapshot.paramMap.get('id');
+    this.TfrId = Number(this.route.snapshot.paramMap.get('id'));
+    if (!Number.isInteger(this.TfrId)) {
+      this.router.navigate(['/home']);
+    }
+    this.route.paramMap.subscribe((result) => {
+      this.TfrId = Number(result.get('id'));
+    });
+    if (!Number.isInteger(this.TfrId)) {
+      this.router.navigate(['/home']);
+    }
   }
 }
