5cbf6fd0833b40973f9acf9e71c919230616de16
ACCOLITE\marek.tomecek <marek.tomecek@accolitedigital.com>
2023-01-03 14:05:18 +0000

    Add Angular Material Form

--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReportsService } from 'src/app/services/reports.service';
 
 @Component({
   selector: 'app-reports',
@@ -6,7 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reports.component.scss'],
 })
 export class ReportsComponent implements OnInit {
-  constructor() {}
+  schedulerObject = {
+    tfr: '',
+    type: '',
+    frequency: '',
+    schedule: '',
+    receiver: '',
+  };
 
-  ngOnInit(): void {}
+  schedulerForm!: FormGroup;
+
+  constructor() {
+    reportsService: ReportsService;
+  }
+
+  ngOnInit(): void {
+    this.schedulerForm = new FormGroup({
+      tfr: new FormControl('', [Validators.required]),
+      type: new FormControl('', [Validators.required]),
+      frequency: new FormControl('', [Validators.required]),
+      schedule: new FormControl('', [Validators.required]),
+      receiver: new FormControl('', [Validators.required]),
+    });
+  }
+
+  submit() {
+    this.schedulerObject.tfr = this.schedulerForm.get('tfr')?.value;
+    this.schedulerObject.type = this.schedulerForm.get('type')?.value;
+    this.schedulerObject.frequency = this.schedulerForm.get('frequency')?.value;
+    this.schedulerObject.schedule = this.schedulerForm.get('schedule')?.value;
+    this.schedulerObject.receiver = this.schedulerForm.get('receiver')?.value;
+  }
 }
