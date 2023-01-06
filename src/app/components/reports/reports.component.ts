aa3c0cae07180da238f57431e09e551cb3d7635a
ACCOLITE\marek.tomecek <marek.tomecek@accolitedigital.com>
2023-01-06 10:44:59 +0000

    Remove Report service, add JSON object for backend calls

--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ReportsService } from 'src/app/services/reports.service';
 import { FrequencyPickerComponent } from '../frequency-picker/frequency-picker.component';
+import { HttpClient } from '@angular/common/http';
 
 interface Template {
   value: string;
@@ -17,47 +17,110 @@ export class ReportsComponent implements OnInit {
   @ViewChild(FrequencyPickerComponent, { static: true })
   frequencyPickerComponent!: FrequencyPickerComponent;
 
-  schedulerObject = {
-    tfr: '',
-    type: '',
-    frequency: '',
-    schedule: '',
-    receiver: '',
+  taskObject = {
+    task: {
+      project_id: 0,
+      task_type: 'ALERT',
+      execute_at: '',
+      recurring: false,
+      cron: null as string | null,
+      by_email: true,
+    },
+    resource_emails: [] as string[],
   };
 
   schedulerForm!: FormGroup;
-
   resourceId!: number;
-
-  tfrDummyList: number[] = [1, 2, 3];
-
+  resourceEmail!: string;
   tfrList: any;
+  resourceList: any;
 
   templates: Template[] = [
     { value: 'ALERT', viewValue: 'Alert' },
     { value: 'REPORT', viewValue: 'Report' },
   ];
 
-  constructor(private reportsService: ReportsService) {}
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
     this.resourceId = 1;
+    this.resourceEmail = 'johnbowers@accolitedigital.com';
 
     this.schedulerForm = new FormGroup({
       tfr: new FormControl(1, [Validators.required]),
-      type: new FormControl('Alert', [Validators.required]),
-      receiver: new FormControl('Self', [Validators.required]),
+      type: new FormControl('ALERT', [Validators.required]),
+      receiver: new FormControl('self', [Validators.required]),
       frequency: this.frequencyPickerComponent.createFormGroup(),
     });
 
-    this.tfrList = this.reportsService.getResourceTFRList(this.resourceId);
-    console.log(this.tfrList);
+    this.getResourceTFRList(this.resourceId);
+    this.getResourcesByTFR(
+      (this.taskObject.task.project_id = this.schedulerForm.get('tfr')?.value)
+    );
   }
 
   submit() {
-    console.log(
-      this.schedulerForm.get('frequency')!.get('startDateControl')?.value,
-      this.frequencyPickerComponent.getCron()
+    this.taskObject.task.project_id = this.schedulerForm.get('tfr')?.value;
+    this.taskObject.task.task_type = this.schedulerForm.get('type')?.value;
+
+    let taskDate = this.schedulerForm
+      .get('frequency')
+      ?.get('startDateControl')?.value;
+
+    console.log('taskdate: ', taskDate.getDate);
+
+    taskDate.setTime(
+      this.schedulerForm.get('frequency')?.get('timeControl')?.value
     );
+
+    console.log(taskDate.toString());
+
+    this.taskObject.task.execute_at = taskDate.toJSON();
+
+    if (
+      this.schedulerForm.get('frequency')?.get('recurringControl')?.value ===
+      true
+    ) {
+      this.taskObject.task.recurring = true;
+      this.taskObject.task.cron = this.frequencyPickerComponent.getCron();
+    }
+
+    this.taskObject.task.by_email = true;
+
+    if (this.schedulerForm.get('receiver')?.value === 'self') {
+      this.taskObject.resource_emails.push(this.resourceEmail);
+      this.taskObject.task.cron = null;
+    } else if (
+      this.schedulerForm.get('receiver')?.value === 'allProjectResources'
+    ) {
+      this.getResourcesByTFR(
+        (this.taskObject.task.project_id = this.schedulerForm.get('tfr')?.value)
+      );
+      this.resourceList.forEach((element: { email: string }) => {
+        this.taskObject.resource_emails.push(element.email);
+      });
+    }
+
+    console.log('logging taskObject', this.taskObject);
+  }
+
+  getResourceTFRList(resourceId: number) {
+    this.httpClient
+      .get(`http://localhost:8080/resources/${resourceId}/projects`)
+      .subscribe((response) => {
+        this.tfrList = response;
+      });
+  }
+
+  getResourcesByTFR(tfrId: number) {
+    this.httpClient
+      .get(`http://localhost:8080/search/resource/project/${tfrId}`)
+      .subscribe((response) => {
+        this.resourceList = response;
+      });
+  }
+
+  createTask(taskObject: any) {
+    this.httpClient.post('localhost:8080/tasks', taskObject);
   }
 }
