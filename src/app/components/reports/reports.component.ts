4ce6446475bab93fe899cef8a9c66e25b0f97e0b
Mani-Yazdankhah <mani.yazdankhah@accolitedigital.com>
2023-01-10 12:54:16 +0000

    resolved conflicts

--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FrequencyPickerComponent } from '../frequency-picker/frequency-picker.component';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
+import { RecieverOptions, templates } from 'src/app/utils';
 
 @Component({
   selector: 'app-reports',
@@ -6,7 +11,108 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reports.component.scss'],
 })
 export class ReportsComponent implements OnInit {
-  constructor() {}
+  @ViewChild(FrequencyPickerComponent, { static: true })
+  frequencyPickerComponent!: FrequencyPickerComponent;
 
-  ngOnInit(): void {}
+  @Input() tfrList: any;
+  @Input() template = templates[0].value;
+  @Input() recieverOption = RecieverOptions.self;
+
+  templates = templates;
+  resource!: any;
+  selectTfrLabelText: string = 'Select TFR';
+  selectTemplateLabelText: string = 'Select Template';
+  submitButtonText: string = 'Schedule';
+  receiverLabelText: string = 'Who do you wish to send reports to?';
+  schedulerFormTfr: number | null = null;
+  recieverOptionsEnum = RecieverOptions;
+  schedulerForm!: FormGroup;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.resource = {
+      id: 1,
+      first_name: 'John',
+      last_name: 'Bowers',
+      type: 'STAFF',
+      email: 'johnbowers@accolitedigital.com',
+      is_deleted: false,
+    };
+
+    this.schedulerForm = new FormGroup({
+      tfr: new FormControl<number | null>(this.tfrList, [Validators.required]),
+      type: new FormControl(this.template, [Validators.required]),
+      receiver: new FormControl(this.recieverOption, [Validators.required]),
+      frequency: this.frequencyPickerComponent.createFormGroup(),
+    });
+
+    this.tfrList = this.getResourceTFRList(this.resource.id);
+  }
+
+  async onSubmit() {
+    const project_id = this.schedulerForm.get('tfr')!.value;
+    const task_type = 'REPORT';
+
+    const [hour, minute] = this.schedulerForm
+      .get('frequency')!
+      .get('timeControl')!
+      .value!.split(':');
+
+    let execute_at: Date = this.schedulerForm
+      .get('frequency')!
+      .get('startDateControl')!.value;
+    execute_at.setHours(hour, minute, 0, 0);
+
+    const recurring = this.schedulerForm
+      .get('frequency')!
+      .get('recurringControl')!.value;
+    const cron = recurring ? this.frequencyPickerComponent.getCron() : null;
+
+    const by_email = true;
+    let resources: any = [];
+
+    switch (this.schedulerForm.get('receiver')!.value) {
+      case RecieverOptions.self:
+        resources = [this.resource];
+        break;
+      case RecieverOptions.allProjectResources:
+        resources = await lastValueFrom(this.getResourcesByTFR(project_id));
+        break;
+      case RecieverOptions.custom:
+        break;
+    }
+
+    this.createTask({
+      task: {
+        project_id: project_id,
+        task_type: task_type,
+        execute_at: execute_at,
+        recurring: recurring,
+        cron: cron,
+        by_email: by_email,
+      },
+      resources: resources,
+    });
+  }
+
+  getResourceTFRList(resourceId: number) {
+    this.httpClient
+      .get(`http://localhost:8080/resources/${resourceId}/projects`)
+      .subscribe((response) => {
+        this.tfrList = response;
+      });
+  }
+
+  getResourcesByTFR(tfrId: number | null) {
+    return this.httpClient
+      .get(`http://localhost:8080/search/resource/project/${tfrId}`)
+      .pipe();
+  }
+
+  createTask(taskObject: any) {
+    this.httpClient
+      .post('http://localhost:8080/tasks', taskObject)
+      .subscribe((response) => console.log(response));
+  }
 }
--- src/app/core-modules/core-material/core-material.module.ts
+++ src/app/core-modules/core-material/core-material.module.ts
@@ -3,7 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule } from '@angular/material/icon';
 
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -18,18 +26,34 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     FlexLayoutModule,
     BrowserAnimationsModule,
+    CommonModule,
     MatIconModule,
-    MatButtonModule,
     MatSidenavModule,
     MatToolbarModule,
-    CommonModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSlideToggleModule,
   ],
   exports: [
     FlexLayoutModule,
     MatIconModule,
-    MatButtonModule,
     MatSidenavModule,
     MatToolbarModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSlideToggleModule,
   ],
 })
 export class CoreMaterialModule {}
