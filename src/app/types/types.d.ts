176d988dd8f7a78b4cc58f8e333744e9801a23e2
Laura Rintoul <laura.rintoul@accolitedigital.com>
2023-01-12 15:52:21 +0000

    made tracker optional for future implementation

--- src/app/types/types.d.ts
+++ src/app/types/types.d.ts
@@ -23,7 +23,7 @@ export interface Milestone {
   delivery_date: Date;
   acceptance_date: Date;
   is_deleted: Boolean;
-  tracker: Tracker;
+  tracker?: Tracker;
 }
 export interface Tracker {
   milestone_id: number;
