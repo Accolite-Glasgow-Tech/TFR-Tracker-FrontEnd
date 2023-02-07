ddd06212c6b9a7a727590e815254a26ea378f628
ACCOLITE\jack.ridley <jack.ridley@accolitedigital.com>
2023-02-07 09:34:00 +0000

    renamed upcoming projects widget upcoming tfrs

--- src/app/components/manage-widget-modal/manage-widget-modal.component.ts
+++ src/app/components/manage-widget-modal/manage-widget-modal.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  OnInit,
-  Inject,
-  Optional,
-} from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, Inject, OnInit, Optional } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
@@ -13,14 +7,13 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./manage-widget-modal.component.scss'],
 })
 export class ManageWidgetModalComponent implements OnInit {
-
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     @Optional() public ref: MatDialogRef<ManageWidgetModalComponent>
   ) {}
 
-  widgetsfalse:any
-  widgets: any=[
+  widgetsfalse: any;
+  widgets: any = [
     {
       componentName: 'Client Location',
       present: true,
@@ -28,50 +21,42 @@ export class ManageWidgetModalComponent implements OnInit {
     {
       componentName: 'Our Clients',
       present: true,
-
     },
     {
       componentName: 'TFR Status',
       present: true,
-
     },
     {
-      componentName: 'Upcoming Projects',
+      componentName: 'Upcoming TFRs',
       present: true,
-
     },
   ];
 
   ngOnInit(): void {
     this.widgetsfalse = this.data.widgetdata;
 
-    for(let i=0;i<this.widgetsfalse.length;i++){
+    for (let i = 0; i < this.widgetsfalse.length; i++) {
       if (this.widgetsfalse[i].componentName === 'Client Location') {
-          this.widgets[0].present=false;   
+        this.widgets[0].present = false;
+      } else if (this.widgetsfalse[i].componentName === 'Our Clients') {
+        this.widgets[1].present = false;
+      } else if (this.widgetsfalse[i].componentName === 'TFR Status') {
+        this.widgets[2].present = false;
+      } else if (this.widgetsfalse[i].componentName === 'Upcoming TFRs') {
+        this.widgets[3].present = false;
       }
-      else if (this.widgetsfalse[i].componentName === 'Our Clients') {
-          this.widgets[1].present=false;        
-      }
-      else if (this.widgetsfalse[i].componentName === 'TFR Status') {
-        this.widgets[2].present=false;        
-      }
-      else if (this.widgetsfalse[i].componentName === 'Upcoming Projects') {
-        this.widgets[3].present=false;  
-      }
-      }
+    }
   }
   // closeWidget(componentName: string) {
   //   this.ref.close(componentName);
   // }
 
-
-  onClick(dat:any){
-  this.ref.close(dat);
-}
+  onClick(dat: any) {
+    this.ref.close(dat);
+  }
 
   isempty(present: boolean) {
     if (present === true) return ['isPresentstyle'];
     return ['notPresentstyle'];
   }
-
 }
