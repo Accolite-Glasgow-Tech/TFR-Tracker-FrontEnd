13a093b74fadedd038482b4e3991ef11369f0809
mtacclt <marek.tomecek@accolitedigital.com>
2023-02-07 14:35:44 +0000

    Change constants to reflect backend changes

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -9,8 +9,8 @@ export const TFRStatusCountURL = `${backendURL}/projects/statusCount`;
 export const clientsURL = `${backendURL}/vendors`;
 export const clientsURLdupe = `${backendURL}/search/clients/all`;
 export const projectSearchURL = `${backendURL}/search/project`;
-export const registrationURL = `${backendURL}/register`;
-export const loginURL = `${backendURL}/login`;
+export const registrationURL = `${backendURL}/users/register`;
+export const loginURL = `${backendURL}/users/login`;
 export const resourceProjectsURL = `${backendURL}/resources/projects`;
 export const TFRCreationResourceURL = `${backendURL}/resources/all/tfr-creation-resource`;
 export const projectsURL = `${backendURL}/projects`;
@@ -19,7 +19,13 @@ export const clientProjectCountURL = `${backendURL}/VendorProjectCount`;
 export const seniorityLevelsURL = `${backendURL}/resources/seniorityLevels`;
 
 export const dateFormat = 'MM/dd/YY';
-export const statusList = ['DRAFT','AGREED', 'IN PROGRESS', 'ARCHIVED', 'DELIVERED'];
+export const statusList = [
+  'DRAFT',
+  'AGREED',
+  'IN PROGRESS',
+  'ARCHIVED',
+  'DELIVERED',
+];
 
 export const daysOfWeek: string[] = [
   'Sunday',
