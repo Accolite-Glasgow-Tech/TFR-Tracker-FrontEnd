c46049448917ed9330ce50139bfd784ae477a295
Laura Rintoul <laura.rintoul@accolitedigital.com>
2023-02-15 12:25:45 +0000

    strong typing of chip

--- src/app/components/chip/chip.component.spec.ts
+++ src/app/components/chip/chip.component.spec.ts
@@ -1,19 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DummyProject } from 'src/app/types/dummy-data';
 import { ChipComponent } from './chip.component';
 
 describe('ChipComponent', () => {
   let component: ChipComponent;
   let fixture: ComponentFixture<ChipComponent>;
-  const testMilestone = {
-    id: 1,
-    project_id: 1,
-    start_date: new Date('01/01/2024'),
-    delivery_date: new Date('01/02/2024'),
-    acceptance_date: new Date('01/02/2024'),
-    is_deleted: false,
-    name: 'test milestone name',
-    description: 'test milestone description',
-  };
+  let testMilestone = DummyProject.milestones[0];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
