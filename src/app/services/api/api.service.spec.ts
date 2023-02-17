78767242020faf40aeb1d86d5685d0bcb81c6caa
Laura Rintoul <laura.rintoul@accolitedigital.com>
2023-02-17 11:24:18 +0000

    fixed api service test imports

--- src/app/services/api/api.service.spec.ts
+++ src/app/services/api/api.service.spec.ts
@@ -1,15 +1,26 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { SnackBarService } from '../snack-bar/snack-bar.service';
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
   let service: ApiService;
+  let snackBarServiceSpy: jasmine.SpyObj<SnackBarService>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ HttpClientTestingModule ],
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: SnackBarService,
+          useValue: jasmine.createSpyObj('SnackBarService', ['showSnackBar']),
+        },
+      ],
     });
     service = TestBed.inject(ApiService);
+    snackBarServiceSpy = TestBed.inject(
+      SnackBarService
+    ) as jasmine.SpyObj<SnackBarService>;
   });
 
   it('should be created', () => {
