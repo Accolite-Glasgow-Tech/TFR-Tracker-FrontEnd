c40f82d0b07a7028b4eeaae40b8e363e590f35c6
RISH_ALI <rrishali@gmail.com>
2023-02-28 13:03:03 +0000

    code refactor

--- src/assets/canvasjs.angular.component.ts
+++ src/assets/canvasjs.angular.component.ts
@@ -10,71 +10,80 @@ https://canvasjs.com/license/
 /*tslint:disable*/
 /*eslint-disable*/
 /*jshint ignore:start*/
-import { Component, AfterViewInit, OnChanges, OnDestroy, Input, Output, EventEmitter } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  Output,
+} from '@angular/core';
 declare var require: any;
 var CanvasJS = require('./canvasjs.min');
 
 @Component({
   selector: 'canvasjs-chart',
-  template: '<div id="{{chartContainerId}}" [ngStyle]="styles"></div>'
+  template: '<div id="{{chartContainerId}}" [ngStyle]="styles"></div>',
 })
-
 class CanvasJSChart implements AfterViewInit, OnChanges, OnDestroy {
-	static _cjsChartContainerId = 0;
-	chart: any;
-	chartContainerId: any;
-	prevChartOptions: any;
-	shouldUpdateChart = false;
+  static _cjsChartContainerId = 0;
+  chart: any;
+  chartContainerId: any;
+  prevChartOptions: any;
+  shouldUpdateChart = false;
 
-	@Input()
-		options: any;
-	@Input()
-		styles: any;
-		
-	@Output()
-		chartInstance = new EventEmitter<object>();
-		
-	constructor() {
-		this.options = this.options ? this.options : {};
-		this.styles = this.styles ? this.styles : { width: "100%", position: "relative" };
-		this.styles.height = this.options.height ? this.options.height + "px" : "200px";
-		
-		this.chartContainerId = 'canvasjs-angular-chart-container-' + CanvasJSChart._cjsChartContainerId++;
-	}
+  @Input()
+  options: any;
+  @Input()
+  styles: any;
 
-	ngDoCheck() {
-		if(this.prevChartOptions != this.options) {
-			this.shouldUpdateChart = true;
-		}
-	}
-	
-	ngOnChanges() {				
-		//Update Chart Options & Render
-		if(this.shouldUpdateChart && this.chart) {
-			this.chart.options = this.options;
-			this.chart.render();
-			this.shouldUpdateChart = false;
-			this.prevChartOptions = this.options;
-		}
-	}
-	
-	ngAfterViewInit() {		
-	  this.chart = new CanvasJS.Chart(this.chartContainerId, this.options);
-	  this.chart.render();
-	  this.prevChartOptions = this.options;
-	  this.chartInstance.emit(this.chart);
-	}
+  @Output()
+  chartInstance = new EventEmitter<object>();
 
-	ngOnDestroy() {
-		if(this.chart)
-			this.chart.destroy();
-	}
+  constructor() {
+    this.options = this.options ? this.options : {};
+    this.styles = this.styles
+      ? this.styles
+      : { width: '100%', position: 'relative' };
+    this.styles.height = this.options.height
+      ? this.options.height + 'px'
+      : '200px';
+
+    this.chartContainerId =
+      'canvasjs-angular-chart-container-' +
+      CanvasJSChart._cjsChartContainerId++;
+  }
+
+  ngDoCheck() {
+    if (this.prevChartOptions != this.options) {
+      this.shouldUpdateChart = true;
+    }
+  }
+
+  ngOnChanges() {
+    //Update Chart Options & Render
+    if (this.shouldUpdateChart && this.chart) {
+      this.chart.options = this.options;
+      this.chart.render();
+      this.shouldUpdateChart = false;
+      this.prevChartOptions = this.options;
+    }
+  }
+
+  ngAfterViewInit() {
+    this.chart = new CanvasJS.Chart(this.chartContainerId, this.options);
+    this.chart.render();
+    this.prevChartOptions = this.options;
+    this.chartInstance.emit(this.chart);
+  }
+
+  ngOnDestroy() {
+    if (this.chart) this.chart.destroy();
+  }
 }
 
-export {
-	CanvasJSChart,
-	CanvasJS
-};
+export { CanvasJSChart, CanvasJS };
 /*tslint:enable*/
 /*eslint-enable*/
 /*jshint ignore:end*/
